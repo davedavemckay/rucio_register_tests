f3ea18c931dde8c7af7e41e3cd9be9519730c56b
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-07-07 13:13:04 +0100

    ready for test2a

--- scripts/DM-55271_test_2a_TEMPLATE.bash
+++ scripts/DM-55271_test_2a_TEMPLATE.bash
@@ -19,9 +19,21 @@ export DATASET="Dataset/LSSTCam/runs/${BUTLER_REPO}/w_2026_23/${PIPELINE_RUN_TIC
 export CONFIG_FILE="rucio_register.cfg"
 echo "Time: $(date +%s.%N) - Starting rucio-register for $TEST_NAME $PIPELINE_RUN_TICKET at $SITE"
 
-# use first 10 dataset types from the butler query-dataset-types command for the given collection
-butler query-dataset-types "$BUTLER_REPO" --collections "$COLLECTION" | tail -n +3 | head -n 10 | while IFS= read -r TYPE; do
-    echo "type $TYPE"
+# use first 20 dataset types from the butler query-dataset-types command for the given collection
+# butler query-dataset-types "$BUTLER_REPO" --collections "$COLLECTION" | tail -n +3 | head -n 20 | while IFS= read -r TYPE; do
+#     echo "type $TYPE"
+
+rucio-register auto-register \
+--root-chain "$COLLECTION" \
+--repo "$BUTLER_REPO" \
+--dry-run \
+--rucio-register-config "$CONFIG_FILE" \
+--log-level DEBUG \
+--max-dataset-types 20 \
+--out-dir "uuids" \
+
+    #
+
 
     # rucio-register data-products \
     # --repo "$BUTLER_REPO" \
@@ -31,9 +43,10 @@ butler query-dataset-types "$BUTLER_REPO" --collections "$COLLECTION" | tail -n 
     # --rucio-register-config "$CONFIG_FILE" \
     # --log-level DEBUG \
     # --chunk-size 30
-done
+
+# done
 result1=$?
-echo "Time: $(date +%s.%N) - Finished rucio-register for $TEST_NAME $PIPELINE_RUN_TICKET at $SITE "
+echo "Time: $(date +%s.%N) - Finished rucio-register auto-register --dryrun for $TEST_NAME $PIPELINE_RUN_TICKET at $SITE "
 
 echo $result1
 if [ "$result1" != "0" ]; then
