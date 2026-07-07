4abfa87a4b86d2308d4dbf3195518d87dc1ded04
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-07-07 18:09:03 +0100

    switch back to weekly before xargs al rucio-register

--- scripts/DM-55271_test_2ab_TEMPLATE.bash
+++ scripts/DM-55271_test_2ab_TEMPLATE.bash
@@ -5,6 +5,7 @@ export COLLECTION="TEMPLATE_COLLECTION"
 export SITE="TEMPLATE_SITE"
 export PIPELINE_RUN_TICKET="TEMPLATE_TICKET"
 export TEST_NAME="DM-55271-test_2ab"
+export TIMESTAMP=$((`date +%s` % 10000))
 
 rucio whoami
 
@@ -15,7 +16,7 @@ rse_root: TEMPLATE_RSE_ROOT
 dtn_url: TEMPLATE_DTN_URL
 EOF
 
-export DATASET="Dataset/LSSTCam/runs/${BUTLER_REPO}/w_2026_23/${PIPELINE_RUN_TICKET}/${SITE}/${TEST_NAME}"
+export DATASET="Dataset/LSSTCam/runs/${BUTLER_REPO}/w_2026_23/${PIPELINE_RUN_TICKET}/${SITE}/${TEST_NAME}/${TIMESTAMP}"
 export CONFIG_FILE="rucio_register.cfg"
 echo "Time: $(date +%s.%N) - Starting rucio-register for $TEST_NAME $PIPELINE_RUN_TICKET at $SITE"
 
@@ -55,7 +56,16 @@ else
     echo "rucio-register $TEST_NAME Succeeded"
 fi
 
-find uuids/ -type f -exec head {} \;
+setup lsst_distrib -t w_2026_23
+eups list -s rucio_register
+
+find uuids/ -type f -print0 | xargs -0 -I {} -n 1 -P 10 bash -c 'rucio-register dataset-list \
+    --repo "$BUTLER_REPO" \
+    --rucio-dataset $DATASET/{} \
+    --rucio-register-config "$CONFIG_FILE" \
+    --log-level DEBUG \
+    --chunk-size 30 \
+    --uuidlist {}'
 
 # head -n 10 "$first_uuid_file"
 
