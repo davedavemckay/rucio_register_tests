afc7c8f3ffcdeb175f844db39e55d55ccdbab44a
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-07-10 12:09:31 +0100

    reduce retries for testing, use 12 cores for overhead

--- scripts/DM-55271_test_3a_TEMPLATE.bash
+++ scripts/DM-55271_test_3a_TEMPLATE.bash
@@ -20,17 +20,17 @@ export DATASET="Dataset/LSSTCam/runs/${BUTLER_REPO}/w_2026_23/${PIPELINE_RUN_TIC
 export CONFIG_FILE="rucio_register.cfg"
 echo "Time: $(date +%s.%N) - Starting rucio-register for $TEST_NAME $PIPELINE_RUN_TICKET at $SITE"
 
-# use first 20 dataset types from the butler query-dataset-types command for the given collection
-# butler query-dataset-types "$BUTLER_REPO" --collections "$COLLECTION" | tail -n +3 | head -n 20 | while IFS= read -r TYPE; do
-#     echo "type $TYPE"
+# use first 10 dataset types from the butler query-dataset-types command for the given collection
+# reduce max-retries for testing purposes
 
 rucio-register auto-register \
 --root-chain "$COLLECTION" \
 --repo "$BUTLER_REPO" \
 --rucio-register-config "$CONFIG_FILE" \
 --log-level INFO \
---max-dataset-types 20 \
---max-workers 10
+--max-dataset-types 10 \
+--max-workers 10 \
+--max-retries 1
 
 result1=$?
 echo "Time: $(date +%s.%N) - Finished rucio-register auto-register --dryrun for $TEST_NAME $PIPELINE_RUN_TICKET at $SITE "
