4421601bd36fb417d200f9dc98c712296d30d4cd
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-07-10 12:17:43 +0100

    because condor

--- scripts/generate_condor_tail.bash
+++ scripts/generate_condor_tail.bash
@@ -4,4 +4,4 @@ job_id=$1
 
 sed "s/TEMPLATE_JOB_ID/$job_id/g" condor_tail_TEMPLATE.sub > condor_tail_${job_id}.sub
 
-condor_submit -f condor_tail_${job_id}.sub
+condor_submit -file condor_tail_${job_id}.sub
