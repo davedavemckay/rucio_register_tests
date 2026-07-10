72c1ba30d14d8a5ba49aa4caf77850bbc89cda0f
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-07-10 19:36:34 +0100

    _ to .

--- scripts/remote_condor_tail.bash
+++ scripts/remote_condor_tail.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 job_id=$1
 sed "s/TEMPLATE_JOB_ID/$job_id/g" condor_tail_TEMPLATE.sub > condor_tail.${job_id}.sub
-tail_job_id=$(condor_submit -file condor_tail_${job_id}.sub | grep "submitted" | awk '{print $NF}' | sed 's/\.//g')
+tail_job_id=$(condor_submit -file condor_tail.${job_id}.sub | grep "submitted" | awk '{print $NF}' | sed 's/\.//g')
 echo condor_tail.${tail_job_id}.out
