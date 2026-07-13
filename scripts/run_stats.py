4c423e5b75fde010384ec3465d0adb12b2ebc69d
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-07-13 15:09:09 +0100

    cput and wall time stats

--- scripts/run_stats.py
+++ scripts/run_stats.py
@@ -48,14 +48,24 @@ elif using_autoregistration_started:
         dataset = rs_line.split()[-6].strip()
         file_count = 0
         failures = 0
+        total_wall_time = 0
+        total_cpu_time = 0
+        first_start_time = None
+        last_end_time = None
         with open(sys.argv[1], 'r') as f:
             lines = f.readlines()
             for line in lines:
                 if dataset in line and "process_batch" in line:       
                     batch_start_time = datetime.fromisoformat(' '.join(line.split()[1:3]))
+                    if first_start_time is None or batch_start_time < first_start_time:
+                        first_start_time = batch_start_time
+                    print(f"atch registration started at: {batch_start_time}")
                     print(f"Batch registration started at: {batch_start_time}")
                     batch_end_time = datetime.fromisoformat(' '.join(rs_line.split()[1:3]))
+                    if last_end_time is None or batch_end_time > last_end_time:
+                        last_end_time = batch_end_time
                     print(f"Batch registration finished at: {batch_end_time}")
+                    total_cpu_time += (batch_end_time - batch_start_time).total_seconds()
                     stats = rs_line.split(dataset)[1].strip(' - ').split(', ')
                     for stat in stats:
                         if "registered" in stat:
@@ -63,11 +73,16 @@ elif using_autoregistration_started:
                         elif "failed" in stat:
                             failures += int(stat.split(':')[1].strip())
                             break
+    print(f"First batch registration started at: {first_start_time}")
+    print(f"Last batch registration finished at: {last_end_time}")
+    assert isinstance(first_start_time, datetime) and isinstance(last_end_time, datetime), "First and last batch start and end times must be datetime objects"
+    total_wall_time = (last_end_time - first_start_time).total_seconds()
     assert isinstance(batch_start_time, datetime) and isinstance(batch_end_time, datetime), "Batch start and end times must be datetime objects"
-    time_delta = (batch_end_time - batch_start_time).total_seconds()
-    registry_rate = float(file_count) / time_delta if time_delta > 0 else 0
+    
+    registry_rate = float(file_count) / total_wall_time if total_wall_time > 0 else 0
     print("In-progress registration stats.")
-    print(f"Total time taken: {time_delta:.6f} seconds")
+    print(f"Total walltime: {total_wall_time:.6f} seconds")
+    print(f"Total cputime: {total_cpu_time:.6f} seconds")
     print(f"Files registered: {file_count}")
     print(f"Failures: {failures}")
     print(f"Registration rate: {registry_rate:.2f} Hz (files/second)")
