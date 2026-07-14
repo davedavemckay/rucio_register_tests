c2aef99f38a99931e6707c454b90844ee5f20a7f
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-07-14 14:54:21 +0100

    allow max_bytes

--- scripts/remote_condor_tail.bash
+++ scripts/remote_condor_tail.bash
@@ -1,5 +1,20 @@
 #!/bin/bash
-job_id=$1
-sed "s/TEMPLATE_JOB_ID/$job_id/g" condor_tail_TEMPLATE.sub > condor_tail.${job_id}.sub
+if [ $* -eq 1 ];
+then
+        job_id=$1
+        max_bytes=1048576
+elif [ $* -eq 2 ];
+then
+        job_id=$1
+        max_bytes=$2
+else
+        echo "Usage: $0 <job-id> [<max-bytes>]"
+        exit 1
+fi
+job_id=$(echo $job_id | awk -F. '{print $0}')
+TEMP=$RANDOM
+sed "s/TEMPLATE_JOB_ID/$job_id/g" condor_tail_TEMPLATE.sub > $TEMP
+sed "s/TEMPLATE_MAX_BYTES/$max_bytes/g" $TEMP > condor_tail.${job_id}.sub
+rm $TEMP
 tail_job_id=$(condor_submit -file condor_tail.${job_id}.sub | grep "submitted" | awk '{print $NF}' | sed 's/\.//g')
 echo condor_tail.${tail_job_id}.out
