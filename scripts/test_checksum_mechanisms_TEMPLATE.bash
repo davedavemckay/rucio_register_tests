cf92124d11486232195af1ee97c90f9ce43edf53
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-07-16 09:34:41 +0100

    Update test_checksum_mechanisms script and template with full URI construction

--- scripts/test_checksum_mechanisms_TEMPLATE.bash
+++ scripts/test_checksum_mechanisms_TEMPLATE.bash
@@ -6,11 +6,15 @@ export SITE="TEMPLATE_SITE"
 export PIPELINE_RUN_TICKET="TEMPLATE_TICKET"
 export TEST_NAME="DM-55271-test_3a"
 export TIMESTAMP=$((`date +%s` % 10000))
+export RSE_ROOT="TEMPLATE_RSE_ROOT"
+export DTN_URL="TEMPLATE_DTN_URL"
 export TEST_FILE="TEMPLATE_TEST_FILE"
+
+export FULL_URI="${DTN_URL}${TEST_FILE}"
 
 echo "Time: $(date +%s.%N) - Starting test for $TEST_NAME"
 
-python "./rucio_register_tests/scripts/test_checksum_mechanisms.py" "$TEST_FILE"
+python3 "./rucio_register_tests/scripts/test_checksum_mechanisms.py" "$FULL_URI"
 
 result1=$?
 echo "Time: $(date +%s.%N) - Finished test for $TEST_NAME"
