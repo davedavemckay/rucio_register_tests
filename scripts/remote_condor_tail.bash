cc63116ca9e6a322bb5a0d277a59e98b7f3afbf6
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-07-22 21:39:48 +0100

    rename

--- scripts/remote_condor_tail.bash
+++ scripts/remote_condor_tail.bash
@@ -13,8 +13,8 @@ else
 fi
 job_id=$(echo $job_id | awk -F. '{print $0}')
 TEMP=$RANDOM
-sed "s/TEMPLATE_JOB_ID/$job_id/g" condor_tail_TEMPLATE.sub > $TEMP
-sed "s/TEMPLATE_MAX_BYTES/$max_bytes/g" $TEMP > condor_tail.${job_id}.sub
+sed "s/TEMPLATE_JOB_ID/$job_id/g" remote_condor_tail_TEMPLATE.sub > $TEMP
+sed "s/TEMPLATE_MAX_BYTES/$max_bytes/g" $TEMP > remote_condor_tail_${job_id}.sub
 rm $TEMP
-tail_job_id=$(condor_submit -file condor_tail.${job_id}.sub | grep "submitted" | awk '{print $NF}' | sed 's/\.//g')
-echo condor_tail.${tail_job_id}.out
+tail_job_id=$(condor_submit -file remote_condor_tail_${job_id}.sub | grep "submitted" | awk '{print $NF}' | sed 's/\.//g')
+echo remote_condor_tail_${tail_job_id}.out
