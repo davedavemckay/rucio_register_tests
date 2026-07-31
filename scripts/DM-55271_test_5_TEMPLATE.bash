7c3435f3c58a12b041720a2f2d10094b927ab9e9
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-07-31 16:04:31 +0100

    adding test_5 - is_new

---
+++ scripts/DM-55271_test_5_TEMPLATE.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+export BUTLER_REPO="dp2_prep"
+export SCOPE="dp2_prep"
+export COLLECTION="TEMPLATE_COLLECTION"
+export SITE="TEMPLATE_SITE"
+export PIPELINE_RUN_TICKET="TEMPLATE_TICKET"
+export TEST_NAME="DM-55271-test_5"
+export TIMESTAMP=$((`date +%s` % 10000))
+export TRANSFER_LIST_YAML="https://raw.githubusercontent.com/davedavemckay/cm_transfer_list/refs/heads/main/cm_transfer_list.yaml"
+
+rucio whoami
+
+cat <<EOF >rucio_register.cfg
+rucio_rse: "${SITE}_BUTLER_DISK"
+scope: "${SCOPE}"
+rse_root: TEMPLATE_RSE_ROOT
+dtn_url: TEMPLATE_DTN_URL
+EOF
+
+export DATASET_PREFIX="Dataset/LSSTCam/runs/${BUTLER_REPO}/w_2026_23/${PIPELINE_RUN_TICKET}/${SITE}/${TEST_NAME}/${TIMESTAMP}"
+export CONFIG_FILE="rucio_register.cfg"
+echo "Time: $(date +%s.%N) - Starting rucio-register for $TEST_NAME $PIPELINE_RUN_TICKET at $SITE"
+
+rucio-register auto-register \
+--root-chain "$COLLECTION" \
+--repo "$BUTLER_REPO" \
+--rucio-register-config "$CONFIG_FILE" \
+--transfer-list "$TRANSFER_LIST_YAML" \
+--dataset-name-prefix "$DATASET_PREFIX" \
+--clear-is-new \
+--log-level DEBUG \
+--max-dataset-types 1000 \
+--chunk-size 500 \
+--split-size 2000 \
+--max-workers 5 \
+--max-retries 8
+
+result1=$?
+echo "Time: $(date +%s.%N) - Finished rucio-register auto-register for $TEST_NAME $PIPELINE_RUN_TICKET at $SITE "
+
+echo $result1
+if [ "$result1" != "0" ]; then
+    echo "rucio-register $TEST_NAME Failed"
+else
+    echo "rucio-register $TEST_NAME Succeeded"
+fi
+
+echo "End Time: $(date +%s.%N)"
+
+exit 0
