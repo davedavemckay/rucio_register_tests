fcedcef631db73679e43a3c58f5a39b76ad6ab6b
Dave McKay <d.mckay@epcc.ed.ac.uk>
2026-09-03 09:36:29 +0100

    explicitly set max-workers to 1 for test 1

--- scripts/DM-55271_test_1_TEMPLATE.bash
+++ scripts/DM-55271_test_1_TEMPLATE.bash
@@ -32,7 +32,8 @@ butler query-dataset-types "$BUTLER_REPO" --collections "$COLLECTION" | tail -n 
     --rucio-register-config "$CONFIG_FILE" \
     --log-level DEBUG \
     --chunk-size 500 \
-    --max-retries 8
+    --max-retries 8 \
+    --max-workers 1
 
     result1=$?
     echo "Time: $(date +%s.%N) - Finished rucio-register for dataset_type $TYPE for $TEST_NAME $PIPELINE_RUN_TICKET at $SITE "
